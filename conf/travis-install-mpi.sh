1771315503d88457845577a46f2c6756fd68ebcd
Jake Bolewski <jakebolewski@gmail.com>
2015-03-23 21:41:02 -0400

    install gfortran for all mpi versions

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -4,9 +4,9 @@
 set -e
 case $1 in
   mpich1) set -x;
-    sudo apt-get install -q mpich-shmem-bin libmpich-shmem1.0-dev;;
+    sudo apt-get install -q gfortran mpich-shmem-bin libmpich-shmem1.0-dev;;
   mpich2) set -x;
-    sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
+    sudo apt-get install -q gfortran mpich2 libmpich2-3 libmpich2-dev;;
   mpich3) set -x;
     sudo apt-get install -q gfortran libcr0 default-jdk;
     wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.1/mpich_3.1-1ubuntu_amd64.deb;
