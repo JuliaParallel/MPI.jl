4b16cc8ef7cd91919876296479e7a29b1f4afa07
Steven G. Johnson <stevenj@mit.edu>
2015-11-11 20:37:57 -0500

    enable precompilation (fix #63)

--- src/MPI.jl
+++ src/MPI.jl
@@ -1,9 +1,18 @@
+__precompile__()
+
 module MPI
 
 using Compat
 
-include("../deps/src/compile-time.jl")
+const depfile = joinpath(dirname(@__FILE__), "..", "deps", "src", "compile-time.jl")
+isfile(depfile) || error("MPI not properly installed. Please run Pkg.build(\"MPI\")")
+include(depfile)
 include("mpi-base.jl")
 include("cman.jl")
+
+function __init__()
+    # need to open libmpi with RTLD_GLOBAL flag for Linux, before any ccall
+    Libdl.dlopen(libmpi, Libdl.RTLD_LAZY|Libdl.RTLD_DEEPBIND|Libdl.RTLD_GLOBAL)
+end
 
 end
