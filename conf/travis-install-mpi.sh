96d2b525ded33def1317a1cd758deda8c8bebc61
Erik Schnetter <schnetter@gmail.com>
2015-11-23 18:53:19 -0500

    Try to fix Darwin OpenMPI build:

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -11,11 +11,12 @@ os=`uname`
 case "$os" in
     Darwin)
         brew update
-	brew upgrade gcc
+        brew upgrade cmake
+	    brew upgrade gcc
         case "$MPI_IMPL" in
             mpich|mpich3)
                 brew install mpich
-                ;; 
+                ;;
             openmpi)
                 brew install openmpi
                 ;;
