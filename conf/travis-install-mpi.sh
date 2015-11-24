6bcf6db3519c531894ec1f98b876ac87901f9f59
Erik Schnetter <schnetter@gmail.com>
2015-11-23 19:13:35 -0500

    Check whether MPI is pre-installed

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -11,8 +11,11 @@ os=`uname`
 case "$os" in
     Darwin)
         brew update
-        brew upgrade cmake
+        # brew upgrade cmake
 	    brew upgrade gcc
+        which mpirun
+        which mpicc
+        ls /usr/bin/mpi*
         case "$MPI_IMPL" in
             mpich|mpich3)
                 brew install mpich
