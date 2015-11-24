71054baa70459ee6b355ae657efe58e404fe8982
Erik Schnetter <schnetter@gmail.com>
2015-11-23 20:52:06 -0500

    Correct Darwin OpenMPI install

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -11,16 +11,13 @@ os=`uname`
 case "$os" in
     Darwin)
         brew update
-        brew upgrade cmake
-	    brew upgrade gcc
+	brew upgrade gcc
         case "$MPI_IMPL" in
             mpich|mpich3)
                 brew install mpich
-                ;;
+                ;; 
             openmpi)
-                # Temporarily build OpenMPI from source, since apparently our
-                # cmake doesn't know how to handle OpenMPI 1.10 yet
-                brew install openmpi --build-from-source
+                brew install openmpi
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
