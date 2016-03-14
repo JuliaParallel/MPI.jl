141da8bbe4022b4af2ad2c8fba428b0ddd99684d
Andreas Noack <andreasnoackjensen@gmail.com>
2016-03-14 14:42:09 -0400

    Try using trusty

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -31,19 +31,19 @@ case "$os" in
         sudo apt-get update -q
         case "$MPI_IMPL" in
             mpich1)
-                sudo apt-get install -q gfortran mpich-shmem-bin libmpich-shmem1.0-dev
+                sudo apt-get install -y gfortran mpich-shmem-bin libmpich-shmem1.0-dev
                 ;;
             mpich2)
-                sudo apt-get install -q gfortran mpich2 libmpich2-3 libmpich2-dev
+                sudo apt-get install -y gfortran mpich2 libmpich2-3 libmpich2-dev
                 ;;
             mpich|mpich3)
-                sudo apt-get install -q gfortran libcr0 default-jdk
-                wget -q http://www.cebacad.net/files/mpich/ubuntu/mpich-3.1/mpich_3.1-1ubuntu_amd64.deb
-                sudo dpkg -i ./mpich_3.1-1ubuntu_amd64.deb
+                sudo apt-get install -y gfortran libcr0 default-jdk hwloc libmpich10 libmpich-dev
+                wget -q http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mpich/mpich_3.0.4-6ubuntu1_amd64.deb
+                sudo dpkg -i ./mpich_3.0.4-6ubuntu1_amd64.deb
                 # rm -f ./mpich_3.1-1ubuntu_amd64.deb
                 ;;
             openmpi)
-                sudo apt-get install -q gfortran openmpi-bin openmpi-common libopenmpi-dev
+                sudo apt-get install -y gfortran openmpi-bin openmpi-common libopenmpi-dev
                 ;;
             *)
                 echo "Unknown MPI implementation: $MPI_IMPL"
