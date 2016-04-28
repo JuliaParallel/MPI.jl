d8286830a4e9cf53560c49da105ada0b4d3096f0
Katie Hyatt <kshyatt@physics.ucsb.edu>
2016-04-28 14:56:22 -0700

    Add tests for serialized isend/irecv

--- test/test_sendrecv.jl
+++ test/test_sendrecv.jl
@@ -106,4 +106,18 @@ rreq = nothing
 sreq = nothing
 gc()
 
+root = 0
+g = x -> x^2 + 2x - 1
+if MPI.Comm_rank(comm) == root + 1
+    done, f, stat = MPI.irecv( root, root+32, comm )
+    if done
+        @test f(3) == g(3)
+        @test f(5) == g(5)
+        @test f(7) == g(7)
+    end
+elseif MPI.Comm_rank(comm) == root
+    f = g
+    sreq = MPI.isend(f, dst, rank+32, comm)
+end
+
 MPI.Finalize()
