578fce3cb511286c23f712929325f5ec4377dab5
Max G <jgoldfar@users.noreply.github.com>
2017-05-05 14:34:48 -0400

    Update build script to fix build on Mac

--- conf/travis-install-mpi.sh
+++ conf/travis-install-mpi.sh
@@ -12,7 +12,6 @@ case "$os" in
     Darwin)
         brew update
         brew upgrade cmake
-	    brew upgrade gcc
         case "$MPI_IMPL" in
             mpich|mpich3)
                 brew install mpich
