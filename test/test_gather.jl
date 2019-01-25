544bfe84e6aafb3c5bf837fc1798332a821bf5d8
Filippo Vicentini <filippovicentini@gmail.com>
2019-01-25 11:26:09 +0100

    Add tests for gather

--- test/test_gather.jl
+++ test/test_gather.jl
@@ -3,25 +3,51 @@ using MPI
 
 MPI.Init()
 
-function gather(A, root)
-    comm = MPI.COMM_WORLD
-    C = MPI.Gather(A, root, comm)
-end
-
 comm = MPI.COMM_WORLD
 root = 0
+rank = MPI.Comm_rank(comm)
+sz   = MPI.Comm_size(comm)
+isroot = MPI.Comm_rank(comm) == root
 
 for typ in Base.uniontypes(MPI.MPIDatatype)
+
+    # Allocating
     A = typ[MPI.Comm_rank(comm) + 1]
-    C = gather(A, root)
+    C = MPI.Gather(A, root, comm)
     if MPI.Comm_rank(comm) == root
         @test typeof(C) === Vector{typ}
         @test C == map(typ, collect(1:MPI.Comm_size(comm)))
     end
+
+    # Allocating, object
     A = convert(typ,MPI.Comm_rank(comm) + 1)
-    C = gather(A, root)
+    C = MPI.Gather(A, root, comm)
     if MPI.Comm_rank(comm) == root
         @test typeof(C) === Vector{typ}
+        @test C == map(typ, collect(1:MPI.Comm_size(comm)))
+    end
+
+    # Allocating, explicit length
+    A = typ[MPI.Comm_rank(comm) + 1, 0]
+    C = MPI.Gather(A, 1, root, comm)
+    if MPI.Comm_rank(comm) == root
+        @test typeof(C) === Vector{typ}
+        @test C == map(typ, collect(1:MPI.Comm_size(comm)))
+    end
+
+    # Allocating, view
+    A = typ[MPI.Comm_rank(comm) + 1, 0]
+    C = MPI.Gather(view(A, 1:1), root, comm)
+    if MPI.Comm_rank(comm) == root
+        @test typeof(C) === Vector{typ}
+        @test C == map(typ, collect(1:MPI.Comm_size(comm)))
+    end
+
+    # Non Allocating
+    A = typ[MPI.Comm_rank(comm) + 1]
+    C = Array{typ}(undef, sz)
+    MPI.Gather!(A, C, length(A), root, comm)
+    if MPI.Comm_rank(comm) == root
         @test C == map(typ, collect(1:MPI.Comm_size(comm)))
     end
 end
