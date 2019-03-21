7d69787cb0df5bf793bcfb509b2e3e62321dd043
Colin Caine <cmc@cs.man.ac.uk>
2019-03-21 19:06:06 +0000

    Correct typos

--- src/mpi-base.jl
+++ src/mpi-base.jl
@@ -485,7 +485,7 @@ end
 """
     Send(obj::T, dest::Integer, tag::Integer, comm::Comm) where T
 
-Complete s blocking send of `obj` to MPI rank `dest` of communicator `comm`
+Complete a blocking send of `obj` to MPI rank `dest` of communicator `comm`
 using with the message tag `tag`.
 """
 function Send(obj::T, dest::Integer, tag::Integer, comm::Comm) where T
@@ -496,7 +496,7 @@ end
 """
     send(obj, dest::Integer, tag::Integer, comm::Comm)
 
-Complete a blocking send of using a serialized version of `obj` to MPI rank
+Complete a blocking send of a serialized version of `obj` to MPI rank
 `dest` of communicator `comm` using with the message tag `tag`.
 """
 function send(obj, dest::Integer, tag::Integer, comm::Comm)
