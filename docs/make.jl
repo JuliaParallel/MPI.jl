c13e93bafc3748fd657571492bfb4949e95587cd
Simon Byrne <simonbyrne@gmail.com>
2019-07-30 10:54:13 -0700

    fix doc paths

--- docs/make.jl
+++ docs/make.jl
@@ -15,18 +15,20 @@ isdir(examples_md_dir) || mkdir(examples_md_dir)
 for (example_title, example_md) in EXAMPLES
     example_jl = example_md[1:end-2]*"jl"
     open(joinpath(@__DIR__, "src", example_md), "w") do mdfile
-         println(mdfile, "# $example_title")
-         println(mdfile)
-         println(mdfile, "`$example_jl`")
-         println(mdfile, "```julia")
-         write(mdfile, read(joinpath(@__DIR__,example_jl)))
-         println(mdfile, "```")
-         println(mdfile)
+        println(mdfile, "# $example_title")
+        println(mdfile)
+        println(mdfile, "`$example_jl`")
+        println(mdfile, "```julia")
+        write(mdfile, read(joinpath(@__DIR__,example_jl)))
+        println(mdfile, "```")
+        println(mdfile)
 
-         println(mdfile, "```")
-         println(mdfile, "> mpiexec -n 3 julia $example_jl")
-         write(mdfile, read(`$(MPI.mpiexec) -n 3 $(joinpath(Sys.BINDIR, Base.julia_exename())) --project $(joinpath(@__DIR__,example_jl))`))
-         println(mdfile, "```")
+        println(mdfile, "```")
+        println(mdfile, "> mpiexec -n 3 julia $example_jl")
+        cd(@__DIR__) do
+            write(mdfile, read(`$(MPI.mpiexec) -n 3 $(joinpath(Sys.BINDIR, Base.julia_exename())) --project $example_jl`))
+        end
+        println(mdfile, "```")
     end
 end
 
