9b6813a3dab456713504be84ccd1abb715c226d0
Valentin Churavy <v.churavy@gmail.com>
2019-08-03 07:04:58 -0400

    disable UCX memory hooks

--- src/MPI.jl
+++ src/MPI.jl
@@ -49,6 +49,13 @@ function __init__()
         Libdl.dlopen(libmpi, Libdl.RTLD_LAZY | Libdl.RTLD_GLOBAL)
     end
 
+    # disable UCX memory hooks since it can mess up dlopen
+    # https://github.com/openucx/ucx/issues/4001
+    ENV["UCX_MEM_MMAP_RELOC"] = "no"
+    ENV["UCX_MEM_MALLOC_HOOKS"] = "no"
+    ENV["UCX_MEM_MALLOC_RELOC"] = "no"
+    ENV["UCX_MEM_EVENTS"] = "no"
+
     if filesize(dlpath(libmpi)) != libmpi_size
         error("MPI library has changed, re-run Pkg.build(\"MPI\")")
     end
