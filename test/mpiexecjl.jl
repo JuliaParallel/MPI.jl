efa3fdcb26f184fe5e743cd0f087a98a0d1214b5
Mosè Giordano <mose@gnu.org>
2020-05-13 19:19:37 +0100

    Fix error in `mpiexecjl` and improve test

--- test/mpiexecjl.jl
+++ test/mpiexecjl.jl
@@ -1,12 +1,16 @@
-using Test
+using Test, Pkg
 using MPI
 
 @testset "mpiexecjl" begin
     mktempdir() do dir
+        # Install MPI locally, so that we can test the `--project` flag to
+        # `mpiexecjl`
+        Pkg.activate(dir)
+        Pkg.add("MPI")
         @test_logs (:info, r"Installing") (:info, r"Done") MPI.install_mpiexecjl(; destdir = dir)
         mpiexecjl = joinpath(dir, "mpiexecjl")
         julia = joinpath(Sys.BINDIR, Base.julia_exename())
         example = joinpath(@__DIR__, "..", "docs", "examples", "01-hello.jl")
-        @test success(`$(mpiexecjl) $(julia) --startup-file=no -q $(example)`)
+        @test success(`$(mpiexecjl) --project=$(dir) $(julia) --startup-file=no -q $(example)`)
     end
 end
