5e4940a2acf578abc8b2211707d999d917cf4515
Mosè Giordano <mose@gnu.org>
2020-05-13 23:04:42 +0100

    Print usage with `-h` and `--help` and with no non-project argument

--- test/mpiexecjl.jl
+++ test/mpiexecjl.jl
@@ -7,10 +7,20 @@ using MPI
         # `mpiexecjl`
         Pkg.activate(dir)
         Pkg.add("MPI")
+        # Test installation
         @test_logs (:info, r"Installing") (:info, r"Done") MPI.install_mpiexecjl(; destdir = dir)
+        # Test a run of mpiexec
         mpiexecjl = joinpath(dir, "mpiexecjl")
         julia = joinpath(Sys.BINDIR, Base.julia_exename())
         example = joinpath(@__DIR__, "..", "docs", "examples", "01-hello.jl")
         @test success(`$(mpiexecjl) --project=$(dir) $(julia) --startup-file=no -q $(example)`)
+        # Test help messages
+        for help_flag in ("-h", "--help")
+            help_message = read(`$(mpiexecjl) --project=$(dir) --help`, String)
+            @test occursin(r"Usage:.*MPIEXEC_ARGUMENTS", help_message)
+        end
+        # Without arguments, or only with the `--project` option, the wrapper will fail
+        @test !success(`$(mpiexecjl) --project=$(dir)`)
+        @test !success(`$(mpiexecjl)`)
     end
 end
