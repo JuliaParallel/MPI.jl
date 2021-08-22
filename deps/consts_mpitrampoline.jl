f981627b1b22ed97e10988d2cf2699c5c141a0e7
Erik Schnetter <schnetter@gmail.com>
2021-08-22 14:56:54 -0400

    Work on loading constant values

--- deps/consts_mpitrampoline.jl
+++ deps/consts_mpitrampoline.jl
@@ -1,5 +1,10 @@
 # https://github.com/pmodels/mpich/blob/master/src/include/mpi.h.in
 
+@show libmpitrampoline
+const libmpitrampoline_handle = Libc.Libdl.dlopen(libmpitrampoline)
+@show libmpitrampoline_handle
+getval(T, sym) = unsafe_load(Ptr{T}(libmpitrampoline_handle, sym))
+
 const MPI_Aint = Int
 const MPI_Count = Int64
 const MPI_Offset = Int64
@@ -10,10 +15,11 @@ const MPI_Status_Error_offset = 4 * (sizeof(Int) == 8 ? 8 : 7)
 const MPI_Status_size = 4 * (sizeof(Int) == 8 ? 10 : 8)
 
 const MPI_Info = Culong
-const MPI_INFO_NULL = unsafe_load(Ptr{Culong}(libmpi.MPI_INFO_NULL))
+const MPI_INFO_NULL = getval(Culong "MPI_INFO_NULL")
+@show MPI_INFO_NULL
 
 const MPI_Win = Culong
-const MPI_WIN_NULL = Cint(536870912)
+const MPI_WIN_NULL = getval(Culong, "MPI_WIN_NULL")
 
 const MPI_Comm = Culong
 const MPI_COMM_NULL = Cint(67108864)
