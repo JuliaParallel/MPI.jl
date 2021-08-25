77da3ea0c23d8332ab304c4cdc8c2eb16e1b9207
Erik Schnetter <schnetter@gmail.com>
2021-08-25 13:23:49 -0400

    Debug test_sendrecv

--- test/test_sendrecv.jl
+++ test/test_sendrecv.jl
@@ -33,6 +33,7 @@ stats = MPI.Waitall!([sreq, rreq])
 stats::Vector{MPI.Status}
 @test rreq isa MPI.Request
 @test sreq isa MPI.Request
+@show MPI.MPI_ANY_SOURCE MPI.MPI_ANY_TAG
 @show stats
 dump(stats)
 @test MPI.Get_source(stats[2]) == src
