b0742361f2e2d26288c4173c67bdcf32889ea68e
Erik Schnetter <schnetter@gmail.com>
2021-08-24 22:20:48 -0400

    Correct MPI_Test test

--- test/test_test.jl
+++ test/test_test.jl
@@ -25,7 +25,7 @@ recv_mesg_expected = ArrayType{Float64}(undef,N)
 fill!(send_mesg, Float64(rank))
 fill!(recv_mesg_expected, Float64(src))
 
-rreq = MPI.Irecv!(recv_mesg, src,  src+32, comm)
+rreq = MPI.Irecv!(recv_mesg, src, src+32, comm)
 sreq = MPI.Isend(send_mesg, dst, rank+32, comm)
 
 reqs = [sreq,rreq]
@@ -35,14 +35,28 @@ reqs = [sreq,rreq]
 for ind in inds
     (onedone,stat) = MPI.Test!(reqs[ind])
     @test onedone
-    @test stat == MPI.STATUS_EMPTY
+    if ind == 2
+        # Only defined for receive
+        @test stat.tag == 0
+        @test stat.source == mod(rank-1, size)
+    end
+    @test stat.error == MPI.SUCCESS
+    count = MPI.Get_count(stat)
+    @test count == N
 end
 
 (done, ind, stats) = MPI.Testany!(reqs)
-if done && ind != 0
+if done && ind != MPI.UNDEFINED
     (onedone,stat) = MPI.Test!(reqs[ind])
     @test onedone
-    @test stat == MPI.STATUS_EMPTY    
+    if ind == 2
+        # Only defined for receive
+        @test stat.tag == 0
+        @test stat.source == mod(rank-1, size)
+    end
+    @test stat.error == MPI.SUCCESS
+    count = MPI.Get_count(stat)
+    @test count == N
 end
 
 MPI.Waitall!(reqs)
