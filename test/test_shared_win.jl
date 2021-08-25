0498f265565d8496b51a5b4345f2da21d702f9a5
Erik Schnetter <schnetter@gmail.com>
2021-08-25 15:52:07 -0400

    Correct test_shared_win

--- test/test_shared_win.jl
+++ test/test_shared_win.jl
@@ -27,33 +27,36 @@ function main()
     MPI.Init()
 
     global_comm = MPI.COMM_WORLD
-    MPI.Comm_size(global_comm) <= 1 && return
 
-    group_comm_id = 1
-    node_rank = MPI.Comm_rank(global_comm) # do this differently in real code
-    node_comm = MPI.Comm_split(global_comm, group_comm_id, node_rank)
-    owner_rank = 1
+    if MPI.Comm_size(global_comm) > 1
 
-    win, shared_arr =
-        mpi_shared_array(node_comm, Float32, (100, 2); owner_rank=owner_rank)
+        group_comm_id = 1
+        node_rank = MPI.Comm_rank(global_comm) # do this differently in real code
+        node_comm = MPI.Comm_split(global_comm, group_comm_id, node_rank)
+        owner_rank = 1
 
-    if node_rank == 0
-        shared_arr[:, 1] .= 1:100
-    elseif node_rank == 1
-        shared_arr[:, 2] .= 901:1000
+        win, shared_arr =
+            mpi_shared_array(node_comm, Float32, (100, 2); owner_rank=owner_rank)
+
+        if node_rank == 0
+            shared_arr[:, 1] .= 1:100
+        elseif node_rank == 1
+            shared_arr[:, 2] .= 901:1000
+        end
+
+        MPI.Barrier(node_comm) # finish writing before reading
+        # check you can see those values on all processes
+        @test all(shared_arr[:, 1] .== 1:100)
+        @test all(shared_arr[:, 2] .== 901:1000)
+        if node_rank <= 1
+            len, elsize_bytes, baseptr = MPI.Win_shared_query(win, owner_rank)
+            @test elsize_bytes == 4
+            @test len == prod(size(shared_arr))*elsize_bytes
+            @test baseptr == pointer(shared_arr)
+        end
+        MPI.free(win)
+
     end
-
-    MPI.Barrier(node_comm) # finish writing before reading
-    # check you can see those values on all processes
-    @test all(shared_arr[:, 1] .== 1:100)
-    @test all(shared_arr[:, 2] .== 901:1000)
-    if node_rank <= 1
-        len, elsize_bytes, baseptr = MPI.Win_shared_query(win, owner_rank)
-        @test elsize_bytes == 4
-        @test len == prod(size(shared_arr))*elsize_bytes
-        @test baseptr == pointer(shared_arr)
-    end
-    MPI.free(win)
 
     MPI.Finalize()
 end
