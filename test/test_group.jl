ed083ac2503d1459ff4372166a0b9fac2a076be9
Erik Schnetter <schnetter@gmail.com>
2021-11-28 20:57:17 -0500

    Don't compare Comparisons with ===

--- test/test_group.jl
+++ test/test_group.jl
@@ -7,15 +7,15 @@ comm = MPI.COMM_WORLD
 grp = MPI.Comm_group(comm)
 @test MPI.Comm_size(comm) == MPI.Group_size(grp)
 @test MPI.Comm_rank(comm) == MPI.Group_rank(grp)
-@test MPI.Group_compare(grp, grp) === MPI.IDENT
+@test MPI.Group_compare(grp, grp) == MPI.IDENT
 grp2 = MPI.Group_union(grp, MPI.GROUP_EMPTY)
-@test MPI.Group_compare(grp, grp2) === MPI.IDENT
+@test MPI.Group_compare(grp, grp2) == MPI.IDENT
 grp3 = MPI.Group_difference(grp, grp2)
-@test MPI.Group_compare(grp3, MPI.GROUP_EMPTY) === MPI.IDENT
+@test MPI.Group_compare(grp3, MPI.GROUP_EMPTY) == MPI.IDENT
 grp4 = MPI.Group_intersection(grp, grp2)
-@test MPI.Group_compare(grp4, MPI.GROUP_EMPTY) === MPI.UNEQUAL
+@test MPI.Group_compare(grp4, MPI.GROUP_EMPTY) == MPI.UNEQUAL
 grp5 = MPI.Group_excl(grp, Int32[0])
-@test MPI.Group_size(grp5) == (MPI.Group_size(grp)-1)
+@test MPI.Group_size(grp5) == MPI.Group_size(grp) - 1
 grp6 = MPI.Group_incl(grp, Int32[0])
 @test MPI.Group_size(grp6) == 1
 
