c908cf48a8519a1fb153a488bbb8e6458405651c
Valentin Churavy <v.churavy@gmail.com>
2022-04-15 12:28:26 -0400

    test hypothesis

--- test/test_bcast.jl
+++ test/test_bcast.jl
@@ -15,10 +15,14 @@ MPI.Init()
 comm = MPI.COMM_WORLD
 
 root = 0
-Random.seed!(17)
 matsize = (17,17)
 
 for T in Base.uniontypes(MPI.MPIDatatype)
+    # This test depends on the stability of the rng and we have observed with
+    # CUDA.jl that it is not gurantueed that the same number of rand calls will
+    # occur on each rank. (This is a hypothesis). To be sure we shall seed the rng
+    # just before we call rand.
+    Random.seed!(17)
     A = ArrayType(rand(T, matsize))
     B = MPI.Comm_rank(comm) == root ? A : similar(A)
     MPI.Bcast!(B, comm; root=root)
