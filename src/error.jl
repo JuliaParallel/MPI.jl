bf55fda8084a64ef29583ceb0482b012e81f8aa5
termi-official <termi-official@users.noreply.github.com>
2022-05-26 15:40:16 +0200

    Eliminate try and catch blocks from mpi check macro in favor of directly working on the library interface.

--- src/error.jl
+++ src/error.jl
@@ -17,22 +17,46 @@ function Base.show(io::IO, err::FeatureLevelError)
     print(io, "FeatureLevelError($(err.function_name)): Minimum version is $(err.min_feature_level)")
 end
 
-macro mpichk(expr, min_ver=v"1.0")
+macro mpichk(expr, min_ver_expr=v"1.0")
+    # This macro can currently just handle ccall expressions.
+    @assert expr isa Expr && expr.head == :call && expr.args[1] == :ccall
+    
+    # A ccall can have either a tuple as its first argument or just the symbol, 
+    # if the called function does not have input arguments.
     if expr.args[2].head == :tuple
         mpi_method_name_meta = expr.args[2].args[1]
+    elseif expr.args[2].head == :symbol
+        mpi_method_name_meta = expr.args[2]
     end
+
+    # Adjust MPI api call to platform-specific details.  
     expr = macroexpand(@__MODULE__, :(@mpicall($expr)))
-    quote 
-        try
+    
+    # Generate the check code
+    quote
+        # Check if MPI library could be found
+        mpihandle = Libc.Libdl.dlopen(libmpi, throw_error=false)
+        if mpihandle == nothing
+            error("Could not open MPI library '$(libmpi)'")
+        end
+
+        # Interpolate symbol from macro into code
+        mpi_method_symbol = $(esc(mpi_method_name_meta))
+
+        # If the symbol can be found in the library we make the actual call to the MPI function
+        if Libc.Libdl.dlsym(mpihandle, mpi_method_symbol, throw_error=false) != nothing
             # MPI_SUCCESS is defined to be 0
             (errcode = $(esc(expr))) == 0 || throw(MPIError(errcode))
-        catch e
-            typeof(e) != ErrorException && rethrow()
-            mpi_method_name = String($(esc(mpi_method_name_meta)))
-            if contains(e.msg, "could not load symbol") && contains(e.msg, mpi_method_name) && MPI.Get_version() < $(esc(min_ver))
-                throw(MPI.FeatureLevelError(mpi_method_name, $(esc(min_ver))))
+        else # Function was not found in MPI library
+            # We first check if the MPI backend does have the minimal version to support the function ...
+            mpi_method_name = String(mpi_method_symbol)
+            min_ver = $(esc(min_ver_expr))
+            cur_ver = MPI.Get_version()
+            if cur_ver < min_ver
+                throw(MPI.FeatureLevelError(mpi_method_name, min_ver)) 
             end
-            rethrow()
+            # ... and give the user some contxt if the version suites, but the function can still not be found.
+            error("Could not call method $(mpi_method_name) for unkown reason. Minimum required version: $min_ver | Version of MPI backend: $cur_ver.")
         end
     end
 end
