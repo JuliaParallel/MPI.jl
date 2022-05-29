e646c0622b3d0b8720632f9284ab0587d4094522
Simon Byrne <simonbyrne@gmail.com>
2022-05-28 22:31:22 -0700

    revert error.jl

--- src/error.jl
+++ src/error.jl
@@ -34,34 +34,8 @@ macro mpichk(expr, min_version=nothing)
     end
 
     expr = macroexpand(@__MODULE__, :(@mpicall($expr)))
-    
-    # Generate the check code
-    quote
-        # Check if MPI library could be found
-        mpihandle = Libc.Libdl.dlopen(libmpi, throw_error=false)
-        if mpihandle == nothing
-            error("Could not open MPI library '$(libmpi)'")
-        end
-
-        # Interpolate symbol from macro into code
-        mpi_method_symbol = $(esc(mpi_method_name_meta))
-
-        # If the symbol can be found in the library we make the actual call to the MPI function
-        if Libc.Libdl.dlsym(mpihandle, mpi_method_symbol, throw_error=false) != nothing
-            # MPI_SUCCESS is defined to be 0
-            (errcode = $(esc(expr))) == 0 || throw(MPIError(errcode))
-        else # Function was not found in MPI library
-            # We first check if the MPI backend does have the minimal version to support the function ...
-            mpi_method_name = String(mpi_method_symbol)
-            min_ver = $(esc(min_ver_expr))
-            cur_ver = MPI.Get_version()
-            if cur_ver < min_ver
-                throw(MPI.FeatureLevelError(mpi_method_name, min_ver)) 
-            end
-            # ... and give the user some contxt if the version suites, but the function can still not be found.
-            error("Could not call method $(mpi_method_name) for unkown reason. Minimum required version: $min_ver | Version of MPI backend: $cur_ver.")
-        end
-    end
+    # MPI_SUCCESS is defined to be 0
+    :((errcode = $(esc(expr))) == 0 || throw(MPIError(errcode)))
 end
 
 function error_string(err::MPIError)
