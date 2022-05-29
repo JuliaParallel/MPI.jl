1bb961edda9de703d747dc665cae29f036a3e8e7
Simon Byrne <simonbyrne@gmail.com>
2022-05-28 22:51:16 -0700

    quote symbol

--- src/error.jl
+++ src/error.jl
@@ -25,10 +25,10 @@ end
 
 macro mpichk(expr, min_version=nothing)
     if !isnothing(min_version) && expr.args[2].head == :tuple
-        fn = expr.args[2].args[1].value
+        fn = expr.args[2].args[1]
         if isnothing(dlsym(libmpi_handle, fn; throw_error=false))
             return quote
-                throw(FeatureLevelError($fn, $min_version))
+                throw(FeatureLevelError($(QuoteNode(fn)), $min_version))
             end
         end
     end
