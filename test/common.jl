10b454cc5b43e1ea5b0d17b358a235b784e9c6eb
Ludovic Raess <ludovic.rass@gmail.com>
2022-05-31 10:32:52 +0200

    Add AMDGPU support to test.

--- test/common.jl
+++ test/common.jl
@@ -5,6 +5,10 @@ if get(ENV,"JULIA_MPI_TEST_ARRAYTYPE","") == "CuArray"
     import CUDA
     ArrayType = CUDA.CuArray
     synchronize() = CUDA.synchronize()
+elseif get(ENV,"JULIA_MPI_TEST_ARRAYTYPE","") == "ROCArray"
+    import AMDGPU
+    ArrayType = AMDGPU.ROCArray
+    synchronize() = nothing
 else
     ArrayType = Array
     synchronize() = nothing
