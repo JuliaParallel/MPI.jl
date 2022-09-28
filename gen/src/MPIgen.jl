6c354e96b8589078367d2ab22f75d4e63f59b09e
t-bltg <tf.bltg@gmail.com>
2022-09-29 01:18:18 +0200

    simplifications

--- gen/src/MPIgen.jl
+++ gen/src/MPIgen.jl
@@ -2,7 +2,7 @@ module MPIgen
     using Clang.Generators
     using MPICH_jll
 
-    generate_signatures(dir=MPICH_jll.artifact_dir) = begin
+    generate_signatures(dir = MPICH_jll.artifact_dir) = begin
         @info "Generate MPI bindings for MPICH in $dir"
 
         out = joinpath(@__DIR__, "..", "out")
@@ -15,11 +15,9 @@ module MPIgen
 
         # add compiler flags, e.g. "-DXXXXXXXXX"
         args = get_default_args()
-        # @show args
         push!(args, "-I$include_dir", "-include$include_dir/mpi.h")
 
         headers = joinpath(include_dir, "mpi_proto.h")
-        # headers = detect_headers(include_dir, args)
 
         # create context
         ctx = create_context(headers, args, options)
@@ -29,8 +27,8 @@ module MPIgen
 
         rm(joinpath(out, "common.jl"))  # remove un-needed file
 
-        lines = String[]
         fn = joinpath(out, "api.jl")
+        lines = String[]
         for line in readlines(fn)
             if startswith(lstrip(line), "ccall")
                 line = replace(line, "ccall" => "@mpichk ccall")
