d857097074fa7f54da7fc7777ed6aa142abd4ac5
Simon Byrne <simonbyrne@gmail.com>
2022-09-30 11:17:04 -0700

    check out AMDGPU master

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, MPI
+using Test, MPI, Pkg
 
 using MPIPreferences
 
@@ -10,6 +10,7 @@ if get(ENV, "JULIA_MPI_TEST_ARRAYTYPE", "") == "CuArray"
     CUDA.precompile_runtime()
     ArrayType = CUDA.CuArray
 elseif get(ENV,"JULIA_MPI_TEST_ARRAYTYPE","") == "ROCArray"
+    Pkg.add(name="AMDGPU", rev="master")
     import AMDGPU
     AMDGPU.versioninfo()
     # DEBUG: currently no `precompile_runtime()` functionnality is implemented in AMDGPU.jl. If needed, it could be added by analogy of CUDA; no use of caps in AMDGPU.jl, but https://github.com/JuliaGPU/AMDGPU.jl/blob/cfaade146977594bf18e14b285ee3a9c84fbc7f2/src/execution.jl#L351-L357 shows how to construct a CompilerJob for a given agent.
