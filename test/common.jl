1eade93dfb5e282825d8523910b95d60e8d1366c
Simon Byrne <simonbyrne@gmail.com>
2022-10-06 14:29:47 -0700

    disable hipDeviceSynchronize, allow soft_fail on AMDGPU

--- test/common.jl
+++ test/common.jl
@@ -12,7 +12,7 @@ elseif get(ENV,"JULIA_MPI_TEST_ARRAYTYPE","") == "ROCArray"
         # TODO: AMDGPU synchronization story is complicated. HSA does not provide a consistent notion of global queues. We need a mechanism for all GPUArrays.jl provided kernels to be synchronized.
         queue = AMDGPU.default_queue()
         barrier = AMDGPU.barrier_and!(queue, AMDGPU.active_kernels(queue))
-        AMDGPU.HIP.hipDeviceSynchronize() # Sync all HIP kernels e.g. BLAS. N.B. this is blocking Julia progress
+        # AMDGPU.HIP.hipDeviceSynchronize() # Sync all HIP kernels e.g. BLAS. N.B. this is blocking Julia progress
         wait(barrier)
     end
 else
