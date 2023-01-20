748af732cb865cb4279e5dc0fe2becbadcb1212f
Mosè Giordano <mose@gnu.org>
2023-01-20 22:30:42 +0000

    Do not load start up file when running tests

--- test/test_spawn.jl
+++ test/test_spawn.jl
@@ -9,7 +9,7 @@ exename = joinpath(Sys.BINDIR, Base.julia_exename())
 @test isfile(exename)
 errors = Vector{Cint}(undef, N-1)
 # Ensure using consistent flags when spawning subprocesses by using same arguments as `Base.julia_cmd`.
-intercomm = MPI.Comm_spawn(exename, vcat(Base.julia_cmd()[2:end], joinpath(@__DIR__, "spawned_worker.jl")), N-1, MPI.COMM_WORLD, errors)
+intercomm = MPI.Comm_spawn(exename, vcat(Base.julia_cmd()[2:end], "--startup-file=no", joinpath(@__DIR__, "spawned_worker.jl")), N-1, MPI.COMM_WORLD, errors)
 @test errors == zeros(Cint,N-1)
 world_comm = MPI.Intercomm_merge(intercomm, false)
 
