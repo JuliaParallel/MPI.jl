3f3d50d287bbcd53a6a298bb81fe8ed5e14d500f
Katharine Hyatt <khyatt@flatironinstitute.org>
2023-11-04 16:40:11 -0400

    Add test for show method

--- test/test_error.jl
+++ test/test_error.jl
@@ -7,5 +7,9 @@ function main()
         error("This is an expected error, ignore it")
     end
     MPI.Barrier(comm)
+    # FeatureLevelError showing
+    v_num = VersionNumber(1,2,3)
+    err = MPI.FeatureLevelError(:my_fake_function, v_num)
+    @test sprintf(show, err) == "FeatureLevelError(:my_fake_function): Minimum MPI version is $v_num"
 end
 main()
