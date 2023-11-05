a1459737acea0bc4df0446f1c338dbcd509453b3
Johannes Blaschke <jpblaschke@lbl.gov>
2023-11-04 19:13:10 -0700

    add non-gpu tests

--- lib/MPIPreferences/test/runtests.jl
+++ lib/MPIPreferences/test/runtests.jl
@@ -4,9 +4,9 @@ project_path = dirname(Pkg.project().path)
 test_dir = @__DIR__
 test_data = joinpath(@__DIR__, "test_data")
 
-cmd(f) = `$(Base.julia_cmd()) --startup-file=no --project=$(project_path) $(joinpath(test_dir, f))`
+cmd(f; a="") = `$(Base.julia_cmd()) --startup-file=no --project=$(project_path) $(joinpath(test_dir, f)) $(a)`
 
-withenv("JULIA_MPI_CC_OPTS_FILE" => joinpath(test_data, "cc_opts_cuda.txt")) do
+function test_generated_prefs(suffix)
     MPIPreferences.use_system_binary(vendor="cray"; mpiexec="srun")
 
     local_pref_1 = joinpath(project_path, "JuliaLocalPreferences.toml")
@@ -15,9 +15,42 @@ withenv("JULIA_MPI_CC_OPTS_FILE" => joinpath(test_data, "cc_opts_cuda.txt")) do
 
     genereated_settings = readchomp(pref_file)
     expected_settings = readchomp(
-        joinpath(test_data, "LocalPreferences_cuda.toml")
+        joinpath(test_data, "LocalPreferences_$(suffix).toml")
     )
 
     @test genereated_settings == expected_settings
-    run(cmd("test_libmpi.jl"))
+    r = run(cmd("test_libmpi.jl"))
+    @test success(r)
 end
+
+function test_existing_prefs(suffix)
+    local_pref_1 = joinpath(project_path, "JuliaLocalPreferences.toml")
+    local_pref_2 = joinpath(project_path, "LocalPreferences.toml")
+
+    # ensure any previous tests have been cleaned up
+    if isfile(local_pref_1) rm(local_pref_1) end
+    if isfile(local_pref_2) rm(local_pref_2) end
+
+    cp(
+        joinpath(test_data, "LocalPreferences_$(suffix).toml"),
+        joinpath(project_path, "LocalPreferences.toml")
+    )
+
+    r = run(cmd("test_libmpi.jl"))
+    @test success(r)
+end
+
+withenv("JULIA_MPI_CC_OPTS_FILE" => joinpath(test_data, "cc_opts_cuda.txt")) do
+    test_generated_prefs("cuda")
+end
+
+withenv("JULIA_MPI_CC_OPTS_FILE" => joinpath(test_data, "cc_opts_cpu.txt")) do
+    test_generated_prefs("cpu")
+end
+
+test_existing_prefs("cuda")
+r = run(cmd("test_gtl_preload.jl"; a="cuda"))
+@test success(r)
+test_existing_prefs("cpu")
+r = run(cmd("test_gtl_preload.jl"; a="cpu"))
+@test success(r)
