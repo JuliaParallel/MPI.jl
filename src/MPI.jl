58a6d6c1634bbf44b5c1e6fb4db82d71d214d51e
Mosè Giordano <mose@gnu.org>
2024-08-26 22:40:25 +0200

    Point MPICH manpages again to "latest" version

--- src/MPI.jl
+++ src/MPI.jl
@@ -19,14 +19,12 @@ end
 
 
 function _doc_external(fname)
-    # Ideally we'd like to use the "latest" version of the docs, but MPICH v4.0
-    # at the moment seems to be the last version with all the docstrings.
-    mpich = "[MPICH](https://www.mpich.org/static/docs/v4.0/www3/$(fname).html)"
+    mpich = "[MPICH](https://www.mpich.org/static/docs/latest/www3/$(fname).html)"
     # All the *_c functions are undocumented in OpenMPI website
     if !endswith(string(fname), "_c")
         openmpi = "[OpenMPI](https://docs.open-mpi.org/en/main/man-openmpi/man3/$(fname).3.html)"
         # Few docstrings aren't available for MPICH
-        if fname in (:MPI_Status_c2f, :MPI_Status_f2c) || startswith(string(fname), "MPI_Type_create_f90_")
+        if startswith(string(fname), "MPI_Type_create_f90_")
             return """
             - `$fname` man page: $(openmpi)
             """
