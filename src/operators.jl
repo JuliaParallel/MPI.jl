80a7df6244613faef13980dd2ff46a9f0c7dcf29
Valentin Churavy <v.churavy@gmail.com>
2024-09-24 09:51:54 +0200

    small fixes

--- src/operators.jl
+++ src/operators.jl
@@ -83,7 +83,9 @@ end
 function (w::OpWrapper{F,T})(_a::Ptr{Cvoid}, _b::Ptr{Cvoid}, _len::Ptr{Cint}, t::Ptr{MPI_Datatype}) where {F,T}
     len = unsafe_load(_len)
     if !isconcretetype(T)
-        T = to_type(Datatype(unsafe_load(t))) # Ptr might actually point to a Julia object so we could unsafe_pointer_to_objref?
+        concrete_T = to_type(Datatype(unsafe_load(t))) # Ptr might actually point to a Julia object so we could unsafe_pointer_to_objref?
+    else
+        concrete_T = T
     end
     function copy(::Type{T}) where T
         @assert isconcretetype(T)
@@ -93,7 +95,7 @@ function (w::OpWrapper{F,T})(_a::Ptr{Cvoid}, _b::Ptr{Cvoid}, _len::Ptr{Cint}, t:
             unsafe_store!(b, w.f(unsafe_load(a,i), unsafe_load(b,i)), i)
         end
     end
-    copy(T)
+    copy(concrete_T)
     return nothing
 end
 
@@ -115,14 +117,14 @@ function Op(f, T=Any; iscommutative=false)
 end
 
 """
-    @RegisterOp(f, T, internal=false)
+    @RegisterOp(f, T)
 
 Register a custom operator [`Op`](@ref) using the function `f` statically.
 On platfroms like AArch64, Julia does not support runtime closures,
-being passed to C. The generic version of [`Op`](@ref) uses that
-to support arbitrary function being passed as MPI reduction operators.
-In contrast `@RegisterOp` can be used to statically declare a function to
-be passed as an MPI operator.
+being passed to C. The generic version of [`Op`](@ref) uses runtime closures
+to support arbitrary functions being passed as MPI reduction operators.
+`@RegisterOp` statically adds a function to the set of functions allowed as
+as an MPI operator.
 
 ```julia
 function my_reduce(x, y)
@@ -134,10 +136,11 @@ MPI.Reduce!(send_arr, recv_arr, my_reduce, MPI.COMM_WORLD; root=root)
 #...
 ```
 !!! warning
-    Note that `@RegisterOp` works be introducing a new method to `Op`, potentially invalidating other users of `Op`.
+    Note that `@RegisterOp` works be introducing a new method of the generic function `Op`.
+    It can only be used as a top-level statement and may trigger method invalidations.
 
 !!! note
-    `T` can be `Any`, but that will lead to a runtime dispatch.
+    `T` can be `Any`, but this will lead to a runtime dispatch.
 """
 macro RegisterOp(f, T)
     name_wrapper = gensym(Symbol(f, :_, T, :_wrapper))
