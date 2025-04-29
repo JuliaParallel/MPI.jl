b1179bb29c199159d21136147c0ac5f399b74ed9
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-04-29 12:03:45 +0100

    Revert "Try to print more?"
    This reverts commit 5728178f806dd1cd180b9f9b5aa02965af4bae9b.

--- test/test_error.jl
+++ test/test_error.jl
@@ -1,19 +1,13 @@
 using MPI
 
-MPI.Init()
-comm = MPI.COMM_WORLD
-rank = MPI.Comm_rank(comm)
-if rank == 1
-    @show get(ENV, "MPI_LOCALNRANKS", nothing)
-    @show get(ENV, "OMPI_COMM_WORLD_SIZE", nothing)
-    @show MPI.Comm_size(comm)
-end
-
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
     rank = MPI.Comm_rank(comm)
     if rank == 1
+        @show get(ENV, "MPI_LOCALNRANKS", nothing)
+        @show get(ENV, "OMPI_COMM_WORLD_SIZE", nothing)
+        @show MPI.Comm_size(comm)
         error("This is an expected error, ignore it")
     end
     MPI.Barrier(comm)
