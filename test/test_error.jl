11ec4ee2b1f072e8e97aba3dbc7a157fcd824b76
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-04-29 12:03:49 +0100

    Revert "Moar debug"
    This reverts commit 6ee7e4121ebc167969e62280d4f81537277c8b81.

--- test/test_error.jl
+++ test/test_error.jl
@@ -1,13 +1,13 @@
 using MPI
 
+@show get(ENV, "MPI_LOCALNRANKS", nothing)
+@show get(ENV, "OMPI_COMM_WORLD_SIZE", nothing)
+
 function main()
     MPI.Init()
     comm = MPI.COMM_WORLD
     rank = MPI.Comm_rank(comm)
     if rank == 1
-        @show get(ENV, "MPI_LOCALNRANKS", nothing)
-        @show get(ENV, "OMPI_COMM_WORLD_SIZE", nothing)
-        @show MPI.Comm_size(comm)
         error("This is an expected error, ignore it")
     end
     MPI.Barrier(comm)
