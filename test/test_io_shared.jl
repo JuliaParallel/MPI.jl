2636fb8a11112970af179799e3cd1046f48e5614
Erik Schnetter <schnetter@gmail.com>
2025-12-12 13:43:51 -0500

    Correct test_io_shared

--- test/test_io_shared.jl
+++ test/test_io_shared.jl
@@ -46,6 +46,7 @@ MPI.Barrier(comm)
 MPI.File.sync(fh)
 
 MPI.File.write_ordered(fh, fill(Int64(rank), rank+1))
+MPI.Barrier(comm)
 @test MPI.File.get_position_shared(fh) == sum(1:sz)
 
 MPI.File.seek_shared(fh, 0)
