b5ef016dcf124706b2ecf380eab3ee5aa5043263
Erik Schnetter <schnetter@gmail.com>
2026-09-09 06:18:27 -0400

    Remove outdated `julia_version >= 1.8` (#981)

--- src/environment.jl
+++ src/environment.jl
@@ -133,21 +133,9 @@ function Init(;threadlevel=:serialized, finalize_atexit=true, errors_return=true
             # MPI_Finalize is a collective and can act like a barrier (this may
             # be implementation specific). If we are terminating due to a Julia
             # exception, we shouldn't call MPI_Finalize.
-            @static if VERSION >= v"1.9-"
-                # In Julia 1.9 we can access the exitcode from the atexit hook
-                atexit() do exitcode
-                    if exitcode == 0 && !Finalized()
-                        Finalize()
-                    end
-                end
-            else
-                # In Julia 1.8 and earlier we can peek at the current exception,
-                # and only if that field is nothing do we terminate.
-                # This does not work in Julia 1.9 or later.
-                atexit() do
-                    if !Finalized() && ccall(:jl_current_exception, Any, ()) === nothing
-                        Finalize()
-                    end
+            atexit() do exitcode
+                if exitcode == 0 && !Finalized()
+                    Finalize()
                 end
             end
         end
